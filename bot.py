018e730b84376763e867e318730cbaef725c1072
Retr0173 <fory71091@gmail.com>
2023-03-27 19:58:48 +0800

    файл Readme

--- bot.py
+++ bot.py
@@ -9,19 +9,23 @@ bot = telebot.TeleBot(token)
 keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 keyboard.add(KeyboardButton('Котика!'))
 
+
 def get_cat():
-	response = requests.get(url)
-	return response.content
+    response = requests.get(url)
+    return response.content
+
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, 
-		"Я тестовый бот!",
-		reply_markup=keyboard)
+    bot.send_message(message.chat.id,
+                     "Я тестовый бот!",
+                     reply_markup=keyboard)
+
 
 @bot.message_handler(regexp='кот')
 def cat_image_message(message):
-	photo = get_cat()
-	bot.send_photo(message.chat.id, photo)
+    photo = get_cat()
+    bot.send_photo(message.chat.id, photo)
 
-bot.infinity_polling() 
+
+bot.infinity_polling()
